6fec984cf1ceb5daac3d4d17a52f8fe9756fb4e2
zhongpeng <859247268@qq.com>
2021-06-19 17:25:35 +0800

    feat: 注册全局异常处理中间件

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const Koa = require("koa");
+const bodyparser = require("koa-bodyparser");
 const InitManager = require("./core/init");
+const catchError = require("./middlewares/exceptions");
 
 const app = new Koa();
 
+// 注册全局异常处理中间件：
+app.use(catchError);
+// 初始化管理器:
 InitManager.initCore(app);
+
+app.use(bodyparser());
 
 app.listen(3000);
--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -2,9 +2,16 @@ const Router = require("koa-router");
 const router = new Router();
 
 router.get("/v1/book/latest", (ctx, next) => {
-  ctx.body = {
-    key: "book",
-  };
+  // ctx.body = {
+  //   key: "book",
+  // };
+
+  // if (true) {
+  //   const error = new global.errs.ParameterException();
+  //   throw error;
+  // }
+
+  throw new Error();
 });
 
 module.exports = router;
