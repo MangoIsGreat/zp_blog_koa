a7b9f4261b3489500cc9f96ba715d2424f43033c
zhongpeng <859247268@qq.com>
2021-06-21 01:37:45 +0800

    feat: 登陆模块-验证账号密码

--- core/http-exception.js
+++ core/http-exception.js
@@ -25,8 +25,28 @@ class Success extends HttpException {
   }
 }
 
+class NotFound extends HttpException {
+  constructor(msg, error_code) {
+    super();
+    this.msg = msg || "资源未找到";
+    this.error_code = error_code || 10000;
+    this.code = 404;
+  }
+}
+
+class AuthFailed extends HttpException {
+  constructor(msg, error_code) {
+    super();
+    this.msg = msg || "授权失败";
+    this.error_code = error_code || 10004;
+    this.code = 401;
+  }
+}
+
 module.exports = {
   HttpException,
   ParameterException,
   Success,
+  NotFound,
+  AuthFailed,
 };
