3e71774f502658f96765edba4acf8806ff5c566a
zhongpeng <859247268@qq.com>
2021-06-21 14:09:04 +0800

    feat: 根据openid创建微信用户

--- config/config.js
+++ config/config.js
@@ -11,4 +11,9 @@ module.exports = {
     secretKey: "abcdefg",
     expiresIn: 60 * 60,
   },
+  wx: {
+    appId: "wx859326cab90073dc",
+    appSecret: "8f1f1357a1fa5746ee0b86aa54f6213e",
+    loginUrl: "https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code",
+  },
 };
