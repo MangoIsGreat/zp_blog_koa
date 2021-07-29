cb5db71024684344b85b921eba57324df522d849
zhongpeng <859247268@qq.com>
2021-07-29 22:40:09 +0800

    feat: 文章评论模块 -- 接口开发

--- app/models/bComment.js
+++ app/models/bComment.js
@@ -11,59 +11,49 @@ const { BReply } = require("./bReply");
 class BComment extends Model {
   // 评论博客
   static async comment(content) {
-    BComment.create(content);
+    await BComment.create(content);
 
     return "评论成功";
   }
 
   static async getList({ blogId }) {
-    // 多表查询(一对多)
-    BComment.belongsTo(User, {
-      foreignKey: "fromId",
-    });
-
-    BComment.belongsTo(BReply, {
-      foreignKey: "id",
-      targetKey: "commentId",
-    });
-
-    BReply.belongsTo(User, {
-      foreignKey: "fromUid",
-    });
-
-    const result = BComment.findAndCountAll({
+    let result = await BComment.findAll({
       where: { blogId },
       attributes: ["id", "blogId", "content", "created_at", "fromId"],
       include: [
         {
-          model: User,
+          model: sequelize.models.User,
+          as: "comment",
           attributes: ["nickname", "id", "avatar", "profession", "signature"],
-        },
-        {
-          model: BReply,
-          include: [
-            {
-              model: User,
-              attributes: [
-                "nickname",
-                "id",
-                "avatar",
-                "profession",
-                "signature",
-              ],
-            },
-          ],
-          // attributes: [
-          //   "blogId",
-          //   "id",
-          //   "commentId",
-          //   "content",
-          //   "fromUid",
-          //   "toUid",
-          // ],
         },
       ],
     });
+
+    result = JSON.parse(JSON.stringify(result));
+
+    for (let i = 0; i < result.length; i++) {
+      let data = await BReply.findAll({
+        where: {
+          commentId: result[i].id,
+        },
+        include: [
+          {
+            model: sequelize.models.User,
+            as: "from",
+            attributes: ["nickname", "id", "avatar", "profession", "signature"],
+          },
+          {
+            model: sequelize.models.User,
+            as: "to",
+            attributes: ["nickname", "id", "avatar", "profession", "signature"],
+          },
+        ],
+      });
+
+      data = JSON.parse(JSON.stringify(data));
+
+      result[i].child = data;
+    }
 
     return result;
   }
@@ -95,6 +85,21 @@ BComment.init(
     tableName: "bcomment",
   }
 );
+
+sequelize.models.BComment.belongsTo(User, {
+  as: "comment",
+  foreignKey: "fromId",
+});
+
+sequelize.models.BReply.belongsTo(User, {
+  as: "from",
+  foreignKey: "fromUid",
+});
+
+sequelize.models.BReply.belongsTo(User, {
+  as: "to",
+  foreignKey: "toUid",
+});
 
 module.exports = {
   BComment,
